5e22126237698c3e05a636a6c98c5bcdd2867400
vinicius4006 <vfranca477@gmail.com>
2022-04-14 17:01:43 -0300

    lista-add

---
+++ lib/components/body/criarProtocolo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EditarProtocolo extends StatelessWidget {
+  const EditarProtocolo({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('Criar'),
+    );
+  }
+}
--- lib/components/home.dart
+++ lib/components/home.dart
@@ -1,18 +1,36 @@
+
 import 'package:flutter/material.dart';
-import 'package:protocolo_app/components/body/exibicaoProtocolo.dart';
+import 'package:protocolo_app/components/body/bodyPage.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
+
+  void _exibirDialogo(BuildContext context){
+    showDialog(context: context, builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Testando'),
+        content: const Text('Testando'),
+        actions: <Widget>[
+          ElevatedButton(onPressed: (){Navigator.of(context).pop();}, child: const Text("Fechar"))
+        ],
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Builder(builder: (BuildContext context) {
+          return IconButton(onPressed: (){_exibirDialogo(context);}, icon: const Icon(Icons.add_outlined));
+        }),
         centerTitle: true,
         title: Text(title),
       ),
+      
       body: const BodyPage(),
     );
   }
